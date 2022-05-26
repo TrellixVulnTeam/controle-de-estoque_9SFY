15753adf62db3074c5ced70805b67d09f52909df
Marcello <marcellorocha632@gmail.com>
2022-05-26 13:17:49 -0300

    Importando e configurando as rotas

--- client/react-controle-de-estoque/src/App.js
+++ client/react-controle-de-estoque/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EstoquePage from './pages/EstoquePage';
 import './App.css';
 
 function App() {
   return (
     <>
-      <EstoquePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EstoquePage />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
